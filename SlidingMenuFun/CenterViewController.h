944bc76003208c89eeda1d70af85dede58de05e3
Natasha Murashev <nmurashev@gmail.com>
2013-06-02 08:43:53 -0500

    initial commit

---
+++ SlidingMenuFun/CenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterViewController.h
+//  SlidingMenuFun
+//
+//  Created by Natasha Murashev on 6/2/13.
+//  Copyright (c) 2013 Natasha Murashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterViewController : UIViewController
+
+@end
---
+++ SlidingMenuFun/CenterViewController.m
@@ -0,0 +1,38 @@
+//
+//  CenterViewController.m
+//  SlidingMenuFun
+//
+//  Created by Natasha Murashev on 6/2/13.
+//  Copyright (c) 2013 Natasha Murashev. All rights reserved.
+//
+
+#import "CenterViewController.h"
+
+@interface CenterViewController ()
+
+@end
+
+@implementation CenterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
